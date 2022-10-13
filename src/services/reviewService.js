ac08d3213391bec219c89ad25eed9a4dfe8196af
Rishi Raj Yadav <Rishiraj.Yadav@ap.jll.com>
2022-10-13 17:34:24 +0530

    Updated property ids.

--- src/services/reviewService.js
+++ src/services/reviewService.js
@@ -28,7 +28,7 @@ const reviews = [
 ];
 const lst_reviews = [
   {
-    propertyId: "ptt1",
+    propertyId: "1",
     review:
       "Came over to the house for a free estimate; arrived on time; came with all of the materials they needed; took their time to do a great job; cleaned up everything before they left so you could not tell they were even here. Everything looks great!",
     rating: 5,
@@ -36,7 +36,7 @@ const lst_reviews = [
     category: "property",
   },
   {
-    propertyId: "ptt1",
+    propertyId: "1",
     review:
       "Thank you Welington Handyman.  You were very accommodating and arrived the SAME day I needed to have new curtains and rods install in two rooms.  Your work was very professional and efficient..  I will definately call upon your services for home repair services again soon",
     rating: 5,
@@ -44,7 +44,7 @@ const lst_reviews = [
     category: "property",
   },
   {
-    propertyId: "ptt1",
+    propertyId: "1",
     review:
       "This is the best place to get all your desires for nails, feet, message, facial etc.  The people are fantastic: kind, helpful, accommodating and excellent. I have been going for years and I have never been disappointed!!  I love the place!",
     rating: 5,
@@ -52,7 +52,7 @@ const lst_reviews = [
     category: "society",
   },
   {
-    propertyId: "ptt1",
+    propertyId: "1",
     review:
       "Clean spot with great service. Accommodated our party of six. I got a gel many pedi with some friends. I added 30 min reflexology, which was great. I didn't get a facial, but I think I will in the future.  I saw they sell Eminence products, which makes me think they must be good facials",
     rating: 4,
@@ -60,7 +60,7 @@ const lst_reviews = [
     category: "society",
   },
   {
-    propertyId: "ptt1",
+    propertyId: "1",
     review:
       "This is the best place to get all your desires for nails, feet, message, facial etc.  The people are fantastic: kind, helpful, accommodating and excellent. I have been going for years and I have never been disappointed!!  I love the place!",
     rating: 3,
@@ -68,7 +68,7 @@ const lst_reviews = [
     category: "locality",
   },
   {
-    propertyId: "ptt1",
+    propertyId: "1",
     review:
       "We got the regular pedi but it felt very rushed. We were done in 30 mins. Maybe I don't get enough pedicures but i was expecting g a little longer. The ladies were nice tho!",
     rating: 2,
@@ -76,7 +76,7 @@ const lst_reviews = [
     category: "locality",
   },
   {
-    propertyId: "ptt1",
+    propertyId: "1",
     review:
       "Simply Green has tons of fun vegan polishes to choose from! Great service every time! Love this place!",
     rating: 4,
@@ -84,7 +84,7 @@ const lst_reviews = [
     category: "owner",
   },
   {
-    propertyId: "ptt1",
+    propertyId: "1",
     review:
       "Clean spot with great service. Accommodated our party of six. I got a gel many pedi with some friends. I added 30 min reflexology, which was great. I didn't get a facial, but I think I will in the future.  I saw they sell Eminence products, which makes me think they must be good facials",
     rating: 4,
