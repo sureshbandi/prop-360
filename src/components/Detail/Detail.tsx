3e3848c1f80fafe2559c33d028d7e5515569172d
Mohit <mohitk.kumar@ap.jll.com>
2022-10-13 16:50:58 +0530

    reviews fetched

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Button from "@mui/material/Button";
 import StarIcon from "@mui/icons-material/Star";
@@ -13,11 +13,16 @@ interface DetailProps {}
 
 const Detail: FC<DetailProps> = () => {
   const { id } = useParams();
-  let prop = getPropertyByPropId(id);
   const [showReview, setShowReview] = useState(false);
-  const revi = getReviewsByPropId(id);
-  console.log({revi});
-  
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    if(reviews.length != getReviewsByPropId(id).length)
+      setReviews(getReviewsByPropId(id))
+  });
+
+  let prop = getPropertyByPropId(id);
 
   const addReview = function(obj: any){
     createReview({...obj, propertyId: id})
@@ -26,7 +31,7 @@ const Detail: FC<DetailProps> = () => {
   return (
     <div className="Detail">
       <div className="review-menu">
-        <LinkTabs reviews ={getReviewsByPropId(id)}/>
+        <LinkTabs reviews ={reviews}/>
       </div>
       <div className="add-review">
         <Button
